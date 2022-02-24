a0076a9879151eb600642f447d7ac2557ada4e27
Robert Albenda <ralbenda@gmail.com>
2022-02-24 10:50:55 -0500

    working on extra credit

--- server.js
+++ server.js
@@ -21,16 +21,30 @@ const Guitarist = sequelize.define("guitarists", {
 const express = require("express");
 const app = express();
 const port = process.env.port || 3000;
-const path = require('path');
+const path = require("path");
 
-app.use('/src', express.static(path.join(__dirname, 'src')))
-app.get('/', (req,res,next) => res.sendFile(path.join(__dirname, 'index.html')));
+app.use("/src", express.static(path.join(__dirname, "src")));
+app.get("/", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "index.html"))
+);
+
+
 
 app.get("/api/guitarists", async (req, res, next) => {
-  try { res.send( await Guitarist.findAll())
+  try {
+    res.send(await Guitarist.findAll());
+  } catch (ex) {
+    next(ex);
   }
-  catch(ex) {
-    next(ex)
+});
+
+app.delete('/api/guitarists/:id', async (req, res, next) => {
+  try {
+    const guitarist = await Guitarist.findByPk(req.params.id);
+    await guitarist.destroy();
+    res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
   }
 });
 
@@ -52,3 +66,63 @@ const init = async () => {
 };
 
 init();
+
+
+/*
+
+  Router.get("/", async (req, res, next) => {
+    try {
+      const employees = await Employee.findAll({
+        include: [Category, { model: Employee, as: "boss" }],
+      });
+  
+      const categories = await Category.findAll();
+      const options = categories
+        .map((category) => {
+          return `
+        <option value='${category.id}'>
+        ${category.name}
+        </option>
+        `;
+        })
+        .join("");
+      const html = employees
+        .map((employee) => {
+          const boss = employee.boss;
+          let bossName = "NO BOSS";
+          if (boss) {
+            bossName = boss.name;
+          }
+          return `<div>
+                          Employee Name: ${employee.name} -- Employee Department: ${employee.category.name} -- Name of Boss: ${bossName}
+                          <a href = '/employees/${employee.id}'> employee page </a>
+                          <a href = 'categories/${employee.categoryId}'> category page </a>
+                      </div>
+                `;
+        })
+        .join("");
+      res.send(`
+      <html>
+          <head>
+            <title> Robbys' Corporation </title>
+          </head>
+          <body>
+            <h1> Robby's Corporation </h1>
+            <form Method = "POST">
+            <input name='name' placeholder = "employee name">
+            <select name="categoryId">
+            ${options}
+            </select>
+            <button> CREATE NEW EMPLOYEE</button>
+            </form>
+            <div> ${html} </div>
+           
+          </body>
+      </html>
+      
+      `);
+    } catch (ex) {
+      next(ex);
+    }
+  });
+*/
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 const ul = document.querySelector("ul");
+ul.addEventListener("click", async (ev) => {
+  if (ev.target.tagName === "LI") {
+    const id = ev.target.getAttribute("data-id");
+    await axios.delete(`/api/guitarists/${id}`);
+    init()
+  }
+});
+
+const button = document.querySelector("button");
+button.addEventListener("click", async(ev) => {
+  if(ev.target.tagName === "BUTTON") {
+    
+  }
+})
+
+const select = document.querySelector("select");
+const option = document.querySelector('option')
+
 const init = async () => {
   const response = await axios.get("/api/guitarists");
   const guitarists = response.data;
   const html = guitarists
     .map((guitarist) => {
-      return ` <li>${guitarist.name} - ${guitarist.style}</li>`;
+      return ` <li data-id='${guitarist.id}'>${guitarist.name} - ${guitarist.style}</li>`;
     })
     .join("");
   ul.innerHTML = html;
+  console.log(guitarists)
+  const options = guitarists.map(guitarist => {
+    return `<options> ${guitarist.style} </options>`
+  }).join('');
+  console.log(options)
+  option.innerHTML = options
 };
+
 init();
